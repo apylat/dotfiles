8496183fd09b715e4285750168dc4f619cede81e
Andrii <andrpylat@gmail.com>
2025-10-25 15:22:33 +0000

    Updates nvim lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -17,26 +17,84 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
+      --------------------------------------------------------------------
+      -- 🟦 TypeScript / JavaScript
+      --------------------------------------------------------------------
+      vim.lsp.config.ts_ls = {
+        default_config = {
+          cmd = { "typescript-language-server", "--stdio" },
+          filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+          root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
+          capabilities = capabilities,
+        },
+      }
+
+      --------------------------------------------------------------------
+      -- 🟥 Ruby (Solargraph)
+      --------------------------------------------------------------------
+      vim.lsp.config.solargraph = {
+        default_config = {
+          cmd = { "solargraph", "stdio" },
+          filetypes = { "ruby" },
+          root_dir = vim.fs.root(0, { "Gemfile", ".git" }),
+          capabilities = capabilities,
+        },
+      }
+
+      --------------------------------------------------------------------
+      -- 🟧 HTML
+      --------------------------------------------------------------------
+      vim.lsp.config.html = {
+        default_config = {
+          cmd = { "vscode-html-language-server", "--stdio" },
+          filetypes = { "html" },
+          root_dir = vim.fs.root(0, { "index.html", ".git" }),
+          capabilities = capabilities,
+        },
+      }
+
+      --------------------------------------------------------------------
+      -- 🟩 Lua
+      --------------------------------------------------------------------
+      vim.lsp.config.lua_ls = {
+        default_config = {
+          cmd = { "lua-language-server" },
+          filetypes = { "lua" },
+          root_dir = vim.fs.root(0, { ".luarc.json", ".git" }),
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+            },
+          },
+        },
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local ft = args.match
+          local configs = {
+            typescript = "ts_ls",
+            typescriptreact = "ts_ls",
+            javascript = "ts_ls",
+            javascriptreact = "ts_ls",
+            ruby = "solargraph",
+            html = "html",
+            lua = "lua_ls",
+          }
+          local server = configs[ft]
+          if server and vim.lsp.get_clients({ name = server })[1] == nil then
+            vim.lsp.start(vim.lsp.config[server].default_config)
+          end
+        end,
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find References" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
     end,
   },
 }
